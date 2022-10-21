f0c57bb8476f8659df2614ee766db402b766c16c
sunrise8vn <sunrise8vn@gmail.com>
2022-10-21 17:47:42 +0700

    Init

---
+++ src/main/java/com/cg/C0522SpbAjaxBankingLocationJwtApplication.java
@@ -0,0 +1,14 @@
+package com.cg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C0522SpbAjaxBankingLocationJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(C0522SpbAjaxBankingLocationJwtApplication.class, args);
+        System.out.println("Application is running.......");
+    }
+
+}
---
+++ src/main/java/com/cg/controller/HomeController.java
@@ -0,0 +1,28 @@
+package com.cg.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public ModelAndView showHomePage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+
+        return modelAndView;
+    }
+
+    @GetMapping("/login")
+    public ModelAndView showLoginPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login");
+
+        return modelAndView;
+    }
+}
